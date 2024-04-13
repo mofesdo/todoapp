33dbe672614bc7a7325de9e5f6239883770fe39c
Modesto Amador <mofesdo.dev@gmail.com>
2024-04-13 03:35:13 -0700

    basic todo list done

--- index.js
+++ index.js
@@ -4,7 +4,18 @@ document.querySelector("#submit").addEventListener("click", add)
 
 function loadList(){
     console.log(localStorage.getItem("count"))
-    
+    let ol = document.querySelector("#list");
+    console.log(ol)
+    if(localStorage.getItem("count") == null){
+        localStorage.setItem("count", 0);
+    }
+    for(let i = 0; i < Number(localStorage.getItem("count")); i++){
+        let li = document.createElement("li")
+        console.log(localStorage.getItem(i));
+        li.appendChild(document.createTextNode(localStorage.getItem(i)))
+        console.log(ol)
+        ol.appendChild(li)
+    }
 }
 
 
@@ -19,6 +30,7 @@ function add(){
     let li = document.createElement("li");
     li.appendChild(document.createTextNode(`${document.querySelector("#task").value}`));
     ul.appendChild(li)
-    let count = ul.children.length;
+    let count = Number(localStorage.getItem("count"));
     localStorage.setItem(count, document.querySelector("#task").value);
+    localStorage.setItem("count", Number(localStorage.getItem("count")) + 1)
 }
