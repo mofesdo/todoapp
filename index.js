d070c98cf54371b7392f37cc41e66181b64d6252
Modesto Amador <mofesdo.dev@gmail.com>
2024-04-12 23:59:27 -0700

    working on localstorage

--- index.js
+++ index.js
@@ -1,16 +1,17 @@
-
+loadList();
 document.querySelector("#clear").addEventListener("click", clearList);
 document.querySelector("#submit").addEventListener("click", add)
 
+function loadList(){
+    console.log(localStorage.getItem("count"))
+    
+}
 
 
 function clearList(){
-    let list = document.querySelector("#list");
-    let listLength = list.children.length;
-    console.log(list.children)
-    while(listLength > 0){
-        list.removeChild(list.childNodes[0])
-    }
+    localStorage.clear();
+    location.reload()
+
 }
 
 function add(){
@@ -18,4 +19,6 @@ function add(){
     let li = document.createElement("li");
     li.appendChild(document.createTextNode(`${document.querySelector("#task").value}`));
     ul.appendChild(li)
+    let count = ul.children.length;
+    localStorage.setItem(count, document.querySelector("#task").value);
 }
