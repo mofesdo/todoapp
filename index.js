2df568034bce48b58fa9e48e43a7a08e1de10bc5
Modesto Amador <mofesdo.dev@gmail.com>
2024-04-13 04:04:53 -0700

    done with to do App

--- index.js
+++ index.js
@@ -13,11 +13,17 @@ function loadList(){
         let li = document.createElement("li")
         console.log(localStorage.getItem(i));
         li.appendChild(document.createTextNode(localStorage.getItem(i)))
+        /* Not fully implemented delete btn
+        let btn = document.createElement("button");
+        btn.classList.add("button");
+        btn.innerHTML = "Erase"
+        btn.addEventListener("click", erase);
+        li.appendChild(btn);
+        */
         console.log(ol)
         ol.appendChild(li)
     }
 }
-
 
 function clearList(){
     localStorage.clear();
@@ -34,3 +40,10 @@ function add(){
     localStorage.setItem(count, document.querySelector("#task").value);
     localStorage.setItem("count", Number(localStorage.getItem("count")) + 1)
 }
+/* Not implemented
+function erase(){
+    console.log("erasing...")
+    localStorage.removeItem(1)
+    localStorage.setItem("count", Number(localStorage.getItem("count")) - 1)
+}
+*/
